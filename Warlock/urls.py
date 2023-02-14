502a6b86436ee6e52dd16cc62a4e52de9755847e
Lutyyyy <ret41977320@hotmail.com>
2023-02-14 19:38:17 +0800

    Write the first page

--- Warlock/urls.py
+++ Warlock/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    # path('game/', include('game.urls')),
+    path('', include('game.urls')),  # Since we have only one app, take the main page as the app page
     path('admin/', admin.site.urls),
 ]
---
+++ game/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from game.views import index, play
+
+urlpatterns = [
+    path("", index, name="game_index"),
+    path("play/", play, name="play"),
+]
